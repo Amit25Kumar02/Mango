7c607aed454f190aadad7ab80349b17886ad0b73
AMIT DHATTERWAL <amitchitain25@gmail.com>
2025-12-08 18:29:53 +0530

    corporatehero page changes done

--- src/components/CorporateTrainingHero.tsx
+++ src/components/CorporateTrainingHero.tsx
@@ -20,7 +20,7 @@ export default function CorporateTrainingHero({
 }) {
   const navigate = useNavigate();
   return (
-    <section className="w-full bg-[#0a0a0c] py-20 px-6 md:px-8 lg:px-60 text-white">
+    <section className="w-full bg-[#0a0a0c] -mb-28 py-20 px-6 md:px-8 lg:px-60 text-white">
       <div className="max-w-7xl mx-auto grid grid-cols-1 lg:grid-cols-2 gap-16 items-center">
 
         {/* LEFT SIDE */}
